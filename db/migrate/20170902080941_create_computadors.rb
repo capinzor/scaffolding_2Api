aaa28ae651576923eea0ba29fbff1191201cb196
capinzor <capinzonr@unal.edu.co>
2017-09-02 03:10:21 -0500

    Modelo computador de nuevo

---
+++ db/migrate/20170902080941_create_computadors.rb
@@ -0,0 +1,12 @@
+class CreateComputadors < ActiveRecord::Migration[5.1]
+  def change
+    create_table :computadors do |t|
+      t.string :marca
+      t.string :procesador
+      t.integer :gigas_de_ram
+      t.integer :gigas_de_capacidad
+
+      t.timestamps
+    end
+  end
+end
